ed5ffc0973b1bdf850c3628cca2bbf8daa5c8cfe
wuaoxiang <os.popen@gmail.com>
2023-08-09 15:18:53 +0800

    add dbg! macro

--- os/src/console.rs
+++ os/src/console.rs
@@ -30,4 +30,28 @@ macro_rules! println {
     }
 }
 
-
+/// copy from https://github.com/rust-lang/rust/blob/1.71.0/library/std/src/macros.rs#L340-L362
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        println!("[{}:{}]", file!(), line!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                println!("[{}:{}] {} = {:#?}",
+                    file!(), line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
